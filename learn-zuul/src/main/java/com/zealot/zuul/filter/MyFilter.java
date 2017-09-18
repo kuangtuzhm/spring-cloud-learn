bd0179a65e948dc71348954cee2fd4d1a1e45931
123 <123@123-PC>
2017-09-18 20:57:00 +0800

    1111

---
+++ learn-zuul/src/main/java/com/zealot/zuul/filter/MyFilter.java
@@ -0,0 +1,60 @@
+package com.zealot.zuul.filter;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+
+@Component
+public class MyFilter extends ZuulFilter {
+	
+	private static Logger log = LoggerFactory.getLogger(MyFilter.class);
+
+	@Override
+	public Object run() {
+		RequestContext ctx = RequestContext.getCurrentContext();
+		HttpServletRequest request = ctx.getRequest();
+		log.info(String.format("%s >>> %s", request.getMethod(), request.getRequestURL().toString()));
+        Object accessToken = request.getParameter("token");
+        if(accessToken == null) {
+            log.warn("token is empty");
+            ctx.setSendZuulResponse(false);
+            ctx.setResponseStatusCode(401);
+            try {
+                ctx.getResponse().getWriter().write("token is empty");
+            }catch (Exception e){}
+
+            return null;
+        }
+        log.info("ok");
+        return null;
+	}
+
+	@Override
+	public boolean shouldFilter() {
+		//是否过滤
+		return true;
+	}
+
+	@Override
+	public int filterOrder() {
+		//过滤顺序，数字越小，优先级越高
+		return 0;
+	}
+
+	@Override
+	public String filterType() {
+		/**
+		 * pre：路由之前
+		 * routing：路由之时
+		 * post： 路由之后
+		 * error：发送错误调用
+		 */
+		return "pre";
+	}
+
+}
